66b5afdbef2a361ae5a43f6ffc37a162066db808
aCute5 <costantini95dario@gmail.com>
2023-01-22 15:47:55 +0100

    printing comics in page no style

--- resources/views/guests/comics/index.blade.php
+++ resources/views/guests/comics/index.blade.php
@@ -1,12 +1,22 @@
-@extends('layouts.base')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comics Database</title>
+</head>
+<body>
+    <h1>Lista dei fumetti</h1>
+    <main>
+             <ul>
+                 @foreach ($comics as $comic)
+                <li>{{ $comic->id }}</li>
+                <li>{{ $comic->title}} </li>
+                <li> {{ $comic ->price }}</li>
+                 @endforeach
+              </ul>
+    </main>
+</body>
+</html>
 
-@section('content')
-     <ul>
-         @foreach ($comics as $comic)
-        <li>{{ $comic->id }}</li>
-        <li>{{ $comic->title}} </li>
-        <li> {{ $comic ->price }}</li>
-         @endforeach
-      </ul>
-
-@endsection
